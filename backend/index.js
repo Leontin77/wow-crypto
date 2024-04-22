e17481829afa5394b8a8115ac6ce630a741d3255
DmytroSokolovskyi <dim4uk@gmail.com>
2024-04-22 13:39:28 +0300

    add branch

--- backend/index.js
+++ backend/index.js
@@ -8,28 +8,26 @@ require('dotenv').config();
 
 const TelegramBot = require("node-telegram-bot-api");
 const TOKEN = "6895025275:AAG-QeuUGnZZ5ZvkkzDJ1BuTWNxrhU2KixE";
-// t.me/mgameWOWBot
-// t.me/mgameWOWBot?game=wowCrypto
-// const server = express();
 const bot = new TelegramBot(TOKEN, {
     polling: true
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 
-const gameName = "mgameWOWBot";
+let gameurl = "https://leontin77.github.io/wow/";
+const gameName = "wowtest";
 // const queries = {};
 
 const app = express();
 // mongoose.connect(config.MONGO_URL);
 app.use(helmet());
+
 // app.use(cors({origin: _configureCors}));
 app.use(rateLimit({
     windowMS: 15 * 60 * 1000,
     max: 100
 }));
 
-// if (config.NODE_ENV === 'dev') {
 const morgan = require('morgan');
 
 app.use(morgan('dev'));
@@ -39,29 +37,37 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
-
-
-
-
 bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
+// bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
 bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
-bot.on("callback_query", function (query) {
-    if (query.game_short_name !== gameName) {
-        bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
-    } else {
-        queries[query.id] = query;
-        let gameurl = "https://accsmarket.com/";
-        bot.answerCallbackQuery({
-            callback_query_id: query.id,
-            url: gameurl
-        });
-    }
+
+bot.onText(/start|game/, (msg) => {
+
+
+
+    bot.send_message(msg.from.id, gameName)
 });
 
 
+bot.onText("", (msg) => {
+    console.log(msg)
+ bot.sendMessage(msg.from.id, "22222")
+}
+);
 
-
-
+bot.on("callback_query", function (query) {
+    console.log(query)
+    if (query.game_short_name !== gameName) {
+        bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
+    } else {
+        // bot.sendMessage( query.from.id,   "https://t.me/mgameWOWBot/wowtest")
+        bot.answerCallbackQuery({
+            callback_query_id: query.id,
+            url: "https://t.me/mgameWOWBot/wowtest"
+        });
+    }
+});
+bot.getChatMenuButton({}, )
 
 app.listen(port,
     // "'0.0.0.0",
